5f0a191a57dfbddb5b0bf038bb3245eb642b5df8
ElSupreme_ <45270643+EllSupreme@users.noreply.github.com>
2020-09-27 13:10:43 +0200

    Update Cosmetiques.java

--- src/main/java/io/supreme/supraapi/interfaces/Cosmetiques.java
+++ src/main/java/io/supreme/supraapi/interfaces/Cosmetiques.java
@@ -1,9 +1,18 @@
 package io.supreme.supraapi.interfaces;
 
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
 public interface Cosmetiques {
 
-    public abstract void spawn(Player player);
+    public abstract void setName(String name);
+
+    public abstract String getName();
+
+    public abstract void spawn(Location location, Player player);
+
+    public abstract void depsawn(Player player);
+
+
 
 }
