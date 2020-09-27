4f0093dd74e195cbbcf9fd567b71695081c69e43
ElSupreme_ <45270643+EllSupreme@users.noreply.github.com>
2020-09-27 11:16:38 +0200

    push

--- src/main/java/io.supreme.supraapi/SupraAPI.java
+++
@@ -1,7 +0,0 @@
-package io.supreme.supraapi;
-
-public class SupraAPI {
-
-
-
-}
---
+++ src/main/java/io/supreme/supraapi/SupraAPI.java
@@ -0,0 +1,15 @@
+package io.supreme.supraapi;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * - Connections
+ * - Utils
+ * - Interfaces
+ */
+
+public class SupraAPI extends JavaPlugin {
+
+
+
+}
