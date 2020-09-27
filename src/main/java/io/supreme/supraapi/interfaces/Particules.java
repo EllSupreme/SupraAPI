6261ca1b5255126ef8c17a972f3bf4ba824cb6d0
ElSupreme_ <45270643+EllSupreme@users.noreply.github.com>
2020-09-27 13:11:40 +0200

    Update Particules.java

--- src/main/java/io/supreme/supraapi/interfaces/Particules.java
+++ src/main/java/io/supreme/supraapi/interfaces/Particules.java
@@ -4,8 +4,6 @@ import org.bukkit.entity.Player;
 
 public interface Particules {
 
-    public final float PI = 3.14159265358979323846f;
-
     public abstract void startParticle(Player player);
 
     public abstract void stopParticle(Player player);
