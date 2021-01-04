e11dbcc3c68ccb05911714ab53ba94c311223583
Nathan-Guillaume <45270643+Nathan-Guillaume@users.noreply.github.com>
2021-01-04 21:13:58 +0100

    Update IKit.java

--- src/main/java/io/nathan/nathanapi/interfaces/kit/IKit.java
+++ src/main/java/io/nathan/nathanapi/interfaces/kit/IKit.java
@@ -1,4 +1,13 @@
 package io.nathan.nathanapi.interfaces.kit;
 
+import org.bukkit.entity.Player;
+
 public interface IKit {
+
+    void giveKit(Player player);
+
+    void getName();
+
+    void getDescription();
+
 }
